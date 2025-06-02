bb9ccf90ebe12324b8aa2b676a844c693b099678
puhova olga <ovp3112@gmail.com>
2025-06-02 22:19:08 +0300

    add first dmn

---
+++ src/main/java/com/example/demo/controller/CheckDmnController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+import com.example.demo.service.CheckDMNService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CheckDmnController {
+
+    private final CheckDMNService checkDMNService;
+
+    public CheckDmnController(CheckDMNService checkDMNService) {
+        this.checkDMNService = checkDMNService;
+    }
+
+    @PostMapping("/start-process")
+    public String startProcess() throws InterruptedException {
+        checkDMNService.startSimpleProcess();
+        return "Процесс запущен!";
+    }
+}
